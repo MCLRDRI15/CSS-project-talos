09d3df2ad41187cc9ba2f4b904aaa4d7c149ca1b
Michael Alejandro Rios Rodriguez <71363790+MCLRDRI15@users.noreply.github.com>
2021-07-07 16:57:27 -0500

    git@github.com:MCLRDRI15/CSS-project-talos.git

--- js/main.js
+++ js/main.js
@@ -1,14 +1,87 @@
-$(document).ready(function(){
-	var height = $('.menu').offset().top;
+$(document).ready(function() {
+    var height = $(".menu").offset().top;
 
-	$(window).on('scroll', function(){
-		if ($(window).scrollTop() > height) {
-			$('.menu').addClass('menu-fixed');
-		}else{
-			$('.menu').removeClass('menu-fixed');
-			$('.nav-menu').removeClass('active');
-			$('.hamburguer').removeClass('active');
-		}
-	});
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > height) {
+            $(".menu").addClass("menufixed");
+        } else {
+            $(".menu").removeClass("menufixed");
+            $(".navmenu").removeClass("active");
+            $(".hamburguer").removeClass("active");
+        }
+    });
 });
 
+const boxes = document.querySelectorAll(".boxes");
+const box = document.querySelectorAll(".box");
+
+box.forEach((bloque, i) => {
+    box[i].addEventListener("click", () => {
+        boxes.forEach((conts, i) => {
+            boxes[i].classList.remove("enable");
+        });
+        boxes[i].classList.add("enable");
+    });
+});
+
+window.addEventListener("load", function() {
+    new Glider(document.querySelector(".testimonial"), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        scrollLock: true,
+        dots: ".points",
+        arrows: {
+            prev: ".previus",
+            next: ".next",
+        },
+        responsive: [{
+            // screens greater than >576px
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                itemWidth: 250,
+                duration: 0.25,
+            },
+        }, ],
+    });
+});
+
+const navItem = document.querySelectorAll('.navitem');
+
+navItem.forEach((item, i) => {
+    navItem[i].addEventListener('click', () => {
+
+        navItem.forEach((item, i) => {
+            navItem[i].classList.remove('activo');
+        });
+        navItem[i].classList.add('activo');
+    });
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navmenu");
+
+hamburger.addEventListener("click", mobileTabMenu);
+
+function mobileTabMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".navlink");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+const contact = document.querySelector(".button2");
+
+contact.addEventListener("click", visited);
+
+function visited() {
+    contact.classList.add("visited");
+}
--- js/paginas.js
+++ js/paginas.js
@@ -1,71 +1,66 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-var principalImage = $('.principal-images').offset().top;
-var descriptions = $('.descriptions').offset().top;
+    var principalImage = $('.principalImages').offset().top;
+    var descriptions = $('.descriptions').offset().top;
 
-$(window).on('scroll', function(){
-	if ($(window).scrollTop() > principalImage && $(window).scrollTop() < descriptions) {	
-		$('#one').addClass('activo');
-	}else{
-		$('#one').removeClass('activo');
-	}
-	});
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > principalImage && $(window).scrollTop() < descriptions) {
+            $('#one').addClass('activo');
+        } else {
+            $('#one').removeClass('activo');
+        }
+    });
 
-var teamSection = $('.team-section').offset().top;
+    var teamSection = $('.teamSection').offset().top;
 
-$(window).on('scroll', function(){
-	if ($(window).scrollTop() > descriptions && $(window).scrollTop() < teamSection) {	
-		$('#two').addClass('activo');
-	}else{
-		$('#two').removeClass('activo');
-	}
-	});
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > descriptions && $(window).scrollTop() < teamSection) {
+            $('#two').addClass('activo');
+        } else {
+            $('#two').removeClass('activo');
+        }
+    });
 
-var portafolio = $('.portfolio-section').offset().top;
+    var portafolio = $('.portfolioSection').offset().top;
 
-$(window).on('scroll', function(){
-	if ($(window).scrollTop() > teamSection && $(window).scrollTop() < portafolio) {	
-		$('#three').addClass('activo');
-		$('#four').addClass('activo');
-	}else{
-		$('#three').removeClass('activo');
-		$('#four').removeClass('activo');
-	}
-	});
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > teamSection && $(window).scrollTop() < portafolio) {
+            $('#three').addClass('activo');
+            $('#four').addClass('activo');
+        } else {
+            $('#three').removeClass('activo');
+            $('#four').removeClass('activo');
+        }
+    });
 
 
-var blog = $('.our-blog-section').offset().top;
+    var blog = $('.ourBlogSection').offset().top;
 
-$(window).on('scroll', function(){
-	if ($(window).scrollTop() > portafolio && $(window).scrollTop() < blog) {	
-		$('#five').addClass('activo');
-	}else{
-		$('#five').removeClass('activo');
-	}
-	});
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > portafolio && $(window).scrollTop() < blog) {
+            $('#five').addClass('activo');
+        } else {
+            $('#five').removeClass('activo');
+        }
+    });
 
-var form = $('.form-section').offset().top;
+    var form = $('.formSection').offset().top;
+    var testimonials = $('.testimonial').offset().top;
 
-	$(window).on('scroll', function(){
-	if ($(window).scrollTop() > blog && $(window).scrollTop() < form) {	
-		$('#six').addClass('activo');
-	}else{
-		$('#six').removeClass('activo');
-	}
-	});
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > blog && $(window).scrollTop() < testimonials) {
+            $('#six').addClass('activo');
+        } else {
+            $('#six').removeClass('activo');
+        }
+    });
 
-	$(window).on('scroll', function(){
-	if ($(window).scrollTop() > form) {	
-		$('#seven').addClass('activo');
-}	else{
-		$('#seven').removeClass('activo');
-	}
-	});
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > testimonials) {
+            $('#seven').addClass('activo');
+        } else {
+            $('#seven').removeClass('activo');
+        }
+    });
 
 });
-
-
-
-
-
-
